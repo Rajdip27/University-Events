23031cd6af42a5b7dad63228662ec56c856f4f3d
Rajdip27 <srajdip920@gmail.com>
2025-09-02 12:58:05 +0600

    add dl 🚀⚡

---
+++ src/UniversityEvents.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UniversityEvents.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+
+    }
+}
---
+++ src/UniversityEvents.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using UniversityEvents.Infrastructure.Data;
+
+namespace UniversityEvents.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<UniversityDbContext>((s, builder) =>
+        {
+            //builder.UseSqlServer(configuration[ApplicationConstants.DefaultConnection]);
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection")).ConfigureWarnings(warnings => warnings.Ignore(RelationalEventId.PendingModelChangesWarning));
+            builder.UseLoggerFactory(s.GetRequiredService<ILoggerFactory>());
+            builder.LogTo(Console.WriteLine, LogLevel.Debug);
+        }, ServiceLifetime.Scoped);
+        return services;
+    }
+}
