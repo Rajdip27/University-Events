2c79f8cde88e228d98249e4bd71124ae5165381b
Rajdip27 <srajdip920@gmail.com>
2025-09-02 16:24:33 +0600

    add Repositories 🚀⚡

---
+++ src/UniversityEvents.Application/Repositories/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using UniversityEvents.Application.ViewModel;
+
+namespace UniversityEvents.Application.Repositories;
+
+public interface ICategoryRepository
+{
+    Task<CategoryVm>GetAllCategoryAsync(CancellationToken cancellationToken = default);
+}
+public class CategoryRepository : ICategoryRepository
+{
+    public Task<CategoryVm> GetAllCategoryAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
