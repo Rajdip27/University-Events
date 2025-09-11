ffaa7117b8df39a0c0a16dd1d22fef71baf32cc3
Rajdip27 <srajdip920@gmail.com>
2025-09-11 16:32:26 +0600

    add NullableExtensions ⚡🚀

---
+++ src/UniversityEvents.Application/Extensions/NullableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace UniversityEvents.Application.Extensions;
+
+public static class NullableExtensions
+{
+    public static T GetValueOrDefault<T>(this T value)
+    {
+        return value == null ? default : value;
+    }
+}
