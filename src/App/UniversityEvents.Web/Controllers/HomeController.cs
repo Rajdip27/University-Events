af27e4242523deabd8dfde25f02d9bc587773230
Rajdip27 <srajdip920@gmail.com>
2025-09-16 17:15:27 +0600

    add Page Not Found 🚀⚡

--- src/App/UniversityEvents.Web/Controllers/HomeController.cs
+++ src/App/UniversityEvents.Web/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace UniversityEvents.Web.Controllers
             return View();
         }
 
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
