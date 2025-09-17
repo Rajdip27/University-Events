74568c90491d3dae2e555ddda52b7ce0eddfac8c
Rajdip27 <srajdip920@gmail.com>
2025-09-17 09:35:02 +0600

    add Error page and Access Denied 🚑🚀⚡

--- src/App/UniversityEvents.Web/Controllers/HomeController.cs
+++ src/App/UniversityEvents.Web/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace UniversityEvents.Web.Controllers
         {
             return View();
         }
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
