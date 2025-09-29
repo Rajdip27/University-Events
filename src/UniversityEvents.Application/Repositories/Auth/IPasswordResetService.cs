dc693c156ebe0172102fad5613c3556f81239719
Rajdip27 <srajdip920@gmail.com>
2025-09-29 12:37:04 +0600

    add fix

---
+++ src/UniversityEvents.Application/Repositories/Auth/IPasswordResetService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.Services.Licensing;
+using System.Security.Cryptography;
+using System.Text;
+using UniversityEvents.Application.Services;
+using static UniversityEvents.Core.Entities.Auth.IdentityModel;
+
+namespace UniversityEvents.Application.Repositories.Auth;
+
+public interface IPasswordResetService
+{
+    Task<OperationResult> GenerateResetCodeAsync(string email);
+    Task<OperationResult> VerifyResetCodeAsync(string email, string code);
+    Task<OperationResult> ResetPasswordAsync(string email, string newPassword);
+}
