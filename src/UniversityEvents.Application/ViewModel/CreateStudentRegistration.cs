901f964ee1cd6e2ed6752caae59dd0d1145b563d
Rajdip27 <srajdip920@gmail.com>
2025-12-03 15:57:32 +0600

    add success

--- src/UniversityEvents.Application/ViewModel/CreateStudentRegistration.cs
+++ src/UniversityEvents.Application/ViewModel/CreateStudentRegistration.cs
@@ -5,13 +5,9 @@ using UniversityEvents.Core.Entities.BaseEntities;
 
 namespace UniversityEvents.Application.ViewModel;
 
-
-public class StudentRegistrationVm : BaseEntity
+public class CreateStudentRegistration:BaseEntity
 {
     public long EventId { get; set; }
-    
-    public EventVm Event { get; set; } = default!;
-
     [Required(ErrorMessage = "Full Name is required.")]
     [StringLength(100, MinimumLength = 2, ErrorMessage = "Full Name must be between 2 and 100 characters.")]
     public string FullName { get; set; } = default!;
@@ -32,11 +28,9 @@ public class StudentRegistrationVm : BaseEntity
     [Required(ErrorMessage = "Department is required.")]
     [StringLength(100, ErrorMessage = "Department name cannot exceed 100 characters.")]
     public string Department { get; set; } = default!;
-
     [StringLength(250)]
     public string PhotoPath { get; set; } = default!;
     public string PaymentStatus { get; set; } = "Pending";
-
     [NotMapped]
     public IFormFile ImageFile { get; set; }
 }
