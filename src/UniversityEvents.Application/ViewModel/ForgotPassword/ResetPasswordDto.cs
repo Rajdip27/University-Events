47e17b79af1c82aebc17d7dd3a474d72d1917848
Rajdip27 <srajdip920@gmail.com>
2026-01-12 10:22:30 +0600

    add toster 🚑🚀

--- src/UniversityEvents.Application/ViewModel/ForgotPassword/ResetPasswordDto.cs
+++ src/UniversityEvents.Application/ViewModel/ForgotPassword/ResetPasswordDto.cs
@@ -1,8 +1,20 @@
-﻿namespace UniversityEvents.Application.ViewModel.ForgotPassword;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityEvents.Application.ViewModel.ForgotPassword;
 
 public class ResetPasswordDto
 {
+    [Required]
+    [EmailAddress]
     public string Email { get; set; } = null!;
-    public string Otp { get; set; } = null!;
+
+    [Required]
+    [StringLength(100, MinimumLength = 6)]
+    [DataType(DataType.Password)]
     public string NewPassword { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+    public string ConfirmPassword { get; set; } = null!;
 }
