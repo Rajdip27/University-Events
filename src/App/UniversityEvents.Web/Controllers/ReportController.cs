e93b6302a56b32b28f5a80f37978eb04d75d2d5d
Rajdip27 <srajdip920@gmail.com>
2026-01-13 11:27:22 +0600

    EventRegistrationReport 🚑🚀

---
+++ src/App/UniversityEvents.Web/Controllers/ReportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniversityEvents.Web.Controllers;
+
+public class ReportController : Controller
+{
+    public IActionResult EventRegistrationReport()
+    {
+        return View();
+    }
+}
